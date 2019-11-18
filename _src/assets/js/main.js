bddd24447196013dad38cce292535692b14e670d
lauragromero <lauragromero@gmail.com>
2019-11-18 19:50:23 +0100

    pull

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,3 +1,16 @@
 'use strict';
 
 console.log('>> Ready :)');
+
+//funcion para bajar con click el menu 
+
+const menuDown = document.querySelector ('.down');
+const menuArrow = document.querySelector ('.click-arrow');
+
+
+function dropMenu (){
+    menuDown.classList.toggle ('hidden');
+    menuArrow.classList.toggle ('animate');
+}
+
+menuArrow.addEventListener ('click', dropMenu);
