6e6547a2ef61ab63d71fbc96a7fb5d6dd7878bf6
lauragromero <lauragromero@gmail.com>
2019-11-26 21:02:28 +0100

    add variables form

---
+++ _src/assets/js/06-variables.js
@@ -0,0 +1,9 @@
+
+const nameValue = name.value;
+const jobValue = position.value;
+const emailValue = inputEmail.value;
+const phonelValue = inputPhone.value;
+const linkedinValue = inputLinkedin.value;
+const githubValue = inputGit.value;
+
+
