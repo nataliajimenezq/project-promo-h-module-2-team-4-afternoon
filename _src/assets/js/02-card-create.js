f0145bb897f6e1552e3c4019e9f85dd4964851cc
nataliajimenezq <snjimenezq@gmail.com>
2019-11-27 20:12:50 +0100

    Add form validation

--- _src/assets/js/02-card-create.js
+++ _src/assets/js/02-card-create.js
@@ -1,11 +1,41 @@
-//boton crear tarjeta
-const shareButton = document.querySelector ('#share_btn');
-const shareForm = document.querySelector ('#share-div');
 
-const openShare = () => {
-    console.log ('holllllll');
+//boton crear tarjeta
+const shareButton = document.querySelector('#share_btn');
+const shareForm = document.querySelector('#share-div');
+const elementError = document.querySelector('.errorMessage');
+const elementInputs = document.querySelectorAll('input');
+const label = document.querySelector('label');
+
+/* const inputEmail = document.querySelector('#email');
+const inputPhone = document.querySelector('#phone');
+const inputLinkedin = document.querySelector('#linkedin'); */
+/* const inputGit = document.querySelector('#git'); 
+
+ */
+const validation = () => {
+  let hasErrors = false;
+  for (let input of elementInputs) {
+    if (input.value === "" && input.type !== 'tel') {
+      hasErrors = true;
+      input.classList.add('error');
+    }
+  };
+  return !hasErrors;
+};
+
+
+const sendInfo = () => {
+  if (validation()) {
+    elementError.innerHTML = '';
+    shareButton.classList.add('disabled');
+    shareButton.setAttribute('disabled', 'true');
     shareForm.classList.remove('hidden');
-    
+
+  } else {
+    elementError.innerHTML = '*Rellene todos los campos obligatorios.';
+  }
 }
 
-shareButton.addEventListener ('click', openShare);
+shareButton.addEventListener('click', sendInfo);
+
+
